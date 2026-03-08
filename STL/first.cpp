8bf2b250fb11ef8ff12be359d74af1558de5d85e
poorviii19 <shuklapoorviswe@gmail.com>
2026-03-08 22:43:00 +0530

    updated

--- STL/first.cpp
+++ STL/first.cpp
@@ -32,13 +32,96 @@ void explainPair(){
 void explainVector(){
 
     // vector is a dynamic memory version of an array
-    vector<int> v;
-    v.push_back(1);
-    v.emplace_back(2);
+    // if already we have decided the size of vector but still want to insert 
+    // more elements then we can do it  because vector have dynamic memory.
 
+    // stl use for this is <vector>
+    vector<int> v;
+    v.push_back(1);   //push back is use to insert element , dunamically increases the size and push the element at back or at selected position
+    v.emplace_back(2);  //same functioning as push_back but generally faster.
+
+    // /vector of pair data type:
+    vector<pair<int,int>>vect;
+    vect.push_back({1,2});
+    vect.emplace_back(1,2);
+
+
+    vector<int>v1(5);  //vector of size 5 initially stores garbage value
+    vector<int>v2(v1);  //vector v1 get copied in v2
+
+    vector<int>v3(5,100);  //vector of size 5 and all fixed values are 100
+    vector<int>v4(v3);   //vector v3 get copied in v4
+
+
+
+    // iterator: points to memory address,  such as v.begin() points the starting elememnt's memory in vector
+    //datastructure<data_type>::iteratoe variable_name = vectorname.begin();
+    vector<int>::iterator it = v.begin();  //here it is pointing to memory
+    it++;
+    cout<<*(it)<<" ";   //in order to access anything(element) that is in shared memory we write it with *;
+
+    it = it+2;   //shifted by 2 positions
+    cout<<*(it)<<" ";
+
+
+    vector<int>::iterator it = v.end();
+
+    // vector<int>::iterator it = v.rend();
+
+    // vector<int>::iterator it = v.rbegin();   these both are not used
+    cout<<v[0]<<" "<<v.at(0);
+
+
+    cout<<v.back()<<" ";
+
+
+    for(vector<int>::iterator it = v.begin();it != v.end(); it++){
+        cout<<*(it)<<" ";
+    }
+
+    //short way:
+    for(auto it = v.begin();it != v.end();it++){
+        cout<<*(it)<<" ";
+    }
+
+    for(auto it:v){
+        cout<<it<<" ";
+    }   //auto automaticaly assigns datatype as vector iterator or to particular datatype
+
+
+
+    // deletion in vector:
+
+    v.erase(v.begin()+1); //to erase single element
+
+    v.erase(v.begin()+2, v.begin()+4);  //Erases elements from something to something-1(index)
+
+
+    // Insert Function
+
+    vector<int> vectt(2,100);   //{100,100}
+    vectt.insert(v.begin(),300);  //{300,100,100}
+    vectt.insert(v.begin()+1,2,10); //{300,10,10,100,100}
+
+    vector<int> copy(2.50);  //{50,50}
+    vectt.insert(vectt.begin(),copy.begin(),copy.end());//{50,50,300,10,10,100,100}
+
+
+    cout<<vectt.size(); //gives the size of vector
+
+    v.pop_back(); //deletes from last
+
+    vectt.swap(v);  // swaps both the vectors
+
+
+    v.clear(); // erases the entire vector;
+
+    cout<<v.empty();  //checks whether empty or not
     
 
 }
+
+
 
 int main(){
 
---
+++ STL/second.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<List>
+
+using namespace std;
+
+void explainList(){
+
+
+
+}
+
+int main(){
+
+    explainList();
+
+    return 0;
+}
