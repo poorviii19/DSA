dbcbbb29b6e984d6c212620fc4786e16d50b5bd1
poorviii19 <shuklapoorviswe@gmail.com>
2026-04-25 23:24:07 +0530

    updates

--- Algorithms/kadanesalgo.cpp
+++ Algorithms/kadanesalgo.cpp
@@ -20,15 +20,41 @@ int n = sizeof(arr)/sizeof(arr[0]);
 int current = arr[0];  //best answer seen overall
 int maxSum = arr[0];  //best sum ending at current index
 
+int start =0 ;
+int end = 0;
+int tempstart = 0;
 for(int i =1;i<n;i++){
     // Either extend previous subarray OR start new one here
-    current = max(arr[i], current+arr[i]);
 
-        // Update overall best
-    maxSum = max(maxSum, current);
+    // current = max(arr[i], current+arr[i]);  //can use if we only need maxsum
+    // Update overall best
+    // maxSum = max(maxSum, current);
+
+
+    //use when we need the subarray as well
+    if(arr[i]>current+arr[i]){
+        current = arr[i];
+        tempstart = i;
+    }
+    else{
+        current = current+arr[i];
+    }
+
+    if(current > maxSum) {
+            maxSum = current;
+            start = tempstart;
+            end = i;
+        }
+
 }
 
-cout<<"Maximum subarray sum: "<<maxSum;
+cout<<"Maximum subarray sum: "<<maxSum<<endl;
+
+cout << "Subarray: [ ";
+    for(int i = start; i <= end; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "]";
 
     return 0;
 }
