a49f3827bac73af04f8e7ef67a267a98f491835f
poorviii19 <shuklapoorviswe@gmail.com>
2026-04-25 21:51:56 +0530

    sliding window

---
+++ Array/slidingWindow.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int arr[] = {2,1,5,1,3,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k = 2;
+
+    int windowSum = 0;
+
+
+    // first window
+    for(int i =0;i<k;i++){
+        windowSum+=arr[i];
+    }
+
+    int maxSum = windowSum;
+    // slide window
+    for(int i = k;i<n;i++){
+        windowSum += arr[i];  //add next
+        windowSum -= arr[i-k]; //remove old
+        maxSum = max(maxSum,windowSum);
+    }
+
+
+    cout<<"Max sum is: "<<maxSum<<endl;
+
+    return 0;
+}
+
