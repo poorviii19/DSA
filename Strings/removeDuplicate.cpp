308fd2c649bdbc4ad02206c09a97f06b6d05cf72
poorviii19 <shuklapoorviswe@gmail.com>
2026-05-06 23:25:16 +0530

    remove duplicates in a straing using frequency array

--- Strings/removeDuplicate.cpp
+++ Strings/removeDuplicate.cpp
@@ -18,9 +18,32 @@ void removeDuplicates(char s[]){
 }
 
 
+//using frequency array:
+void removeDuplicates1(char s[]) {
+    int freq[256] = {0};
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        freq[(int)s[i]]++;
+    }
+    int writeIdx = 0;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (freq[(int)s[i]] > 0) {
+            s[writeIdx++] = s[i];
+            freq[(int)s[i]] = 0;  // mark as used
+        }
+    }
+
+    s[writeIdx] = '\0';
+    cout<<s<<endl;
+}
+
+
 int main(){
     char str[] = "Programming";
+    char str1[] = "Programming";
     removeDuplicates(str);
+    removeDuplicates1(str1);
 
     return 0;
 }
