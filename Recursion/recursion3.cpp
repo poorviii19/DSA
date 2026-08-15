bba49097e6c564a7bed695f7ebb286348ebd2980
poorviii19 <shuklapoorviswe@gmail.com>
2026-08-15 15:28:15 +0530

    basics of tail recursion and head recursion

--- Recursion/recursion3.cpp
+++ Recursion/recursion3.cpp
@@ -23,21 +23,41 @@ int power(int n, int m){
 }
 
 
+void print(int n){
+
+    // base case
+    if(n==0){
+        return;
+    }
+
+    // Recursive relation:
+    
+    // tail recursion : performing main logic before recursive call
+    // cout<<n<<endl;
+    // print(n-1);
+    
+
+    // head recursion: performing recursive call before main logic
+    print(n-1);
+    cout<<n<<endl;
+}
+
 int main(){
 
     int n;
     cin>>n;
 
 
-    int ans = fibonaci(n);
-    cout<<ans;
+    // int ans = fibonaci(n);
+    // cout<<ans;
 
 
-    int m;
-    cin>>m;
-    int result = power(n,m);
-    cout<<result;
+    // int m;
+    // cin>>m;
+    // int result = power(n,m);
+    // cout<<result;
 
+    print(n);
 
     return 0;
 }
